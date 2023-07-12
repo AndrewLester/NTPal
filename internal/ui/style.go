cd2396d09541c1e5cf708ebb961e97c33ba1757f
AndrewLester <alester220@gmail.com>
2023-07-12 18:50:53 -0500

    Rename ui --> style

--- internal/ui/style.go
+++ internal/ui/style.go
@@ -2,5 +2,23 @@ package ui
 
 import "github.com/charmbracelet/lipgloss"
 
-var TitleStyle = lipgloss.NewStyle().Inline(true).Bold(true).Foreground(lipgloss.Color("252")).Render
-var HelpStyle = lipgloss.NewStyle().Inline(true).Foreground(lipgloss.Color("241")).Render
+// Colors
+var (
+	TextWhite = lipgloss.Color("252")
+	TextGray  = lipgloss.Color("241")
+
+	TableGray = lipgloss.Color("240")
+)
+
+// Text
+var (
+	Title = lipgloss.NewStyle().Inline(true).Bold(true).Foreground(TextWhite).Render
+	Help  = lipgloss.NewStyle().Inline(true).Foreground(TextGray).Render
+)
+
+// Models
+var (
+	TableBase = lipgloss.NewStyle().
+		BorderStyle(lipgloss.NormalBorder()).
+		BorderForeground(TableGray).Render
+)
